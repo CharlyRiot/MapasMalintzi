c5dceee75574ba34c0de8178214aa38be715969b
CharlyRiot <biosistem@gmail.com>
2025-08-26 12:08:21 -0600

    listeners agregados

--- sw.js
+++ sw.js
@@ -9,11 +9,12 @@ const ASSETS = [
   "./img/icon-192.png",
   "./img/icon-512.png"
 ];
-
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))
@@ -34,5 +35,4 @@ self.addEventListener("fetch", (e) => {
   );
 });
 
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
+
